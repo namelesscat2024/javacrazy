4afd7ed6621df6816b1f2831e4dcb86d40109074
namelesscat2024 <1967533487@qq.com>
2024-01-23 21:35:12 +0800

    Create IfTest.java
    if语句

---
+++ IfTest.java
@@ -0,0 +1,12 @@
+public class IfTest
+{
+   public static void main(String[] args)
+   {
+        var age = 30;
+        if (age > 20)
+        {
+             System.out.println("年龄已经大于20岁了");
+             System.out.println("20岁以上的人应该学会承担责任...");
+          }
+      }
+}
