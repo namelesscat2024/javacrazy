5da9680aace04ed284b70342c8036e8cd8e299a0
namelesscat2024 <1967533487@qq.com>
2024-01-28 11:48:10 +0800

    4.3

---
+++ 04/4.3/NestedLoopTest.java
@@ -0,0 +1,13 @@
+public class NestedLoopTest
+{
+    public static void main(String[] args)
+    {
+        for (var i=0;i<5;i++)
+        {
+            for (var j=0;j<3;j++)
+            {
+                System.out.println("i的值为:"  +i+   " j的值为:"  +j);
+            }
+        }
+    }
+}
